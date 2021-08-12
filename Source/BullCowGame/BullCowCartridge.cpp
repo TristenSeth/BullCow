b5b5e085a50072231b7cab7e14716daadf1865c0
root <root@DESKTOP-0BDT6PR.localdomain>
2021-08-11 21:35:53 -0700

    Rudementary game initilization

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
-{
+void UBullCowCartridge::BeginPlay() {
+    // When the game starts
     Super::BeginPlay();
+    //print welcome
+    PrintLine(TEXT("Hello! Welcome to Cows and Bulls. You need to guess the word before running out of lives."));
+    PrintLine(TEXT("Guess the 4 letter word."));
+    PrintLine(TEXT("Press enter to continue..."));
+
+    SetupGame();
+
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
+// When the player hits enter
+void UBullCowCartridge::OnInput(const FString& Input) {
+    ClearScreen();
+    //check input
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("You Guessed correct!"));
+    } else {
+        PrintLine(TEXT("You guessed wrong! Loser!"));
+    }
+    
+}
 
+void UBullCowCartridge::SetupGame() {
+    HiddenWord = TEXT("Cake");
+    Lives = 4;
+    
 }
