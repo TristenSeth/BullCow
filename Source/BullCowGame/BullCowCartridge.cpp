583cdd27dd79ce464d625c4d532f2d611280c1dc
root <root@DESKTOP-0BDT6PR.localdomain>
2021-08-13 16:44:55 -0700

    Lives depend on length of hidden word

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,27 +6,67 @@ void UBullCowCartridge::BeginPlay() {
     Super::BeginPlay();
     //print welcome
     PrintLine(TEXT("Hello! Welcome to Cows and Bulls. You need to guess the word before running out of lives."));
-    PrintLine(TEXT("Guess the 4 letter word."));
-    PrintLine(TEXT("Press enter to continue..."));
-
     SetupGame();
 
 }
 
 // When the player hits enter
 void UBullCowCartridge::OnInput(const FString& Input) {
-    ClearScreen();
-    //check input
-    if (Input == HiddenWord) {
-        PrintLine(TEXT("You Guessed correct!"));
+    /* 
+        if game is over or lives gone, clear screen and setup game again
+        else, we check the PlayerGuess
+    */
+    if (bGameOver || Lives <= 0) {
+        ClearScreen();
+        SetupGame();
     } else {
-        PrintLine(TEXT("You guessed wrong! Loser!"));
+        ProcessGuess(Input);
     }
-    
 }
 
 void UBullCowCartridge::SetupGame() {
-    HiddenWord = TEXT("Cake");
-    Lives = 4;
+    HiddenWord = TEXT("CakeE");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+    //bGameOver = false;
+    PrintLine(FString::Printf(TEXT("Guess the %i letter word."), Lives));
+    PrintLine(FString::Printf(TEXT("You have %i lives."), Lives));
+    PrintLine(TEXT("Press enter to continue..."));
+
+     //for debugging
+    PrintLine(FString::Printf(TEXT("Hidden Word: %s."), *HiddenWord)); 
+}
+
+void UBullCowCartridge::EndGame() {
+    bGameOver = true;
+    PrintLine(TEXT("Please press enter to continue..."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess) {
+        //check input
+        if (Guess.Len() != HiddenWord.Len()) {
+            PrintLine(TEXT("Please input a word of proper length."));
+            return;
+        }
+        if (Guess == HiddenWord) {
+            PrintLine(TEXT("You Guessed correct!"));
+            EndGame();
+            return;
+        } 
+
+        //incorrect guess. Decrement a life and let them know.
+        PrintLine(TEXT("You guessed wrong! This cost you one life."));
+        --Lives;
+        if (Lives == 0) {
+            bGameOver = true;
+            EndGame();
+            return;
+        }
+        PrintLine(FString::Printf(TEXT("You have %i lives remaining."), Lives));
     
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const {
+    
+    return true;
 }
